baac170c9e6dade3aabbfd83c49fbc9461b2cf7f
Pratikshaben <87810902+PratikshaVyas93@users.noreply.github.com>
2022-07-18 19:33:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='johnlewis_web_scraper',
+    version='0.0.1',
+    description='Package allows you to scrape data about the mobile products on johnlewis.com',
+    url='https://github.com/PratikshaVyas93/Datacollection_pipeline_johnlewis',
+    author='Pratiksha',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['pandas', 'selenium'],
+)
