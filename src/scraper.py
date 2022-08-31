ba5eb7a367d53cf3a3589fc9745cfe2e89a50a7a
Pratiksha <pratiksha@192.168.1.190>
2022-08-31 21:13:49 +0100

    checking the docker action works

--- src/scraper.py
+++ src/scraper.py
@@ -34,7 +34,7 @@ class Scraper():
 
     """
     def __init__(self): 
-        print("Starting....")
+        print("Start scraping the website..")
         driver_path ="/Users/pratiksha/Documents/scratch/Datacollection_pipeline_johnlewis/src/geckodriver"
         #('/Users/pratiksha/Documents/scratch/Datacollection_pipeline_johnlewis/src/geckodriver')
         self.driver_path = driver_path
